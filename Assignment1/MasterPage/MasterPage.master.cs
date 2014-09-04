40999aca209bf2a6dad6031fee226fef9903e5cc
simon <paramanandbhat14@gmail.com>
2014-09-04 15:22:28 -0700

    Master Page

---
+++ Assignment1/MasterPage/MasterPage.master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MeltionHotel
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Today;
+            Calendar1.TodaysDate = today;
+            Calendar1.SelectedDate = Calendar1.TodaysDate;
+
+        }
+
+        protected void JoinButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Registration.aspx");
+        }
+
+
+
+    }
+}
